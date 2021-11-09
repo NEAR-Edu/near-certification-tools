298bfd80bb7813b33e6c15b8295aa49f6298e2c6
Jacob <encody@users.noreply.github.com>
2021-11-09 17:09:50 -0600

    base files + some standard nft implementation

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use near_contract_standards::non_fungible_token::metadata::{
+    NFTContractMetadata, NonFungibleTokenMetadataProvider,
+};
+use near_contract_standards::non_fungible_token::{NonFungibleToken, Token, TokenId};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::{near_bindgen, AccountId, BorshStorageKey, PanicOnDefault};
+use near_sdk::{Promise, PromiseOrValue};
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    NonFungibleToken,
+    Metadata,
+    TokenMetadata,
+    Enumeration,
+    Approval,
+}
+
+#[near_bindgen]
+#[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    token: NonFungibleToken,
+    metadata: LazyOption<NFTContractMetadata>,
+}
+
+#[near_bindgen]
+impl Contract {
+    #[init]
+    pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
+        Self {
+            token: NonFungibleToken::new(
+                StorageKey::NonFungibleToken,
+                owner_id,
+                Some(StorageKey::TokenMetadata),
+                Some(StorageKey::Enumeration),
+                Some(StorageKey::Approval),
+            ),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+        }
+    }
+}
+
+near_contract_standards::impl_non_fungible_token_core!(Contract, token);
+near_contract_standards::impl_non_fungible_token_approval!(Contract, token);
+near_contract_standards::impl_non_fungible_token_enumeration!(Contract, token);
+
+#[near_bindgen]
+impl NonFungibleTokenMetadataProvider for Contract {
+    fn nft_metadata(&self) -> NFTContractMetadata {
+        self.metadata.get().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {}
