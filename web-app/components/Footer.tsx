513346b66c631a8a45c7f395b40e4b9e9d6d222f
ozan isgor <ozanisgor11@gmail.com>
2022-02-10 14:08:28 +0300

    update: darker style of the icons

--- web-app/components/Footer.tsx
+++ web-app/components/Footer.tsx
@@ -64,7 +64,7 @@ export default function Example() {
         </nav>
         <div className="mt-8 flex justify-center space-x-6">
           {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <a key={item.name} href={item.href} className="text-gray-600 hover:text-gray-800">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
