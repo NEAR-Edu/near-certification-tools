840f0dfba54ead08f9920284b75b17a7d5741369
ozan isgor <ozanisgor11@gmail.com>
2022-02-14 21:14:30 +0300

    update: move social one place and add font awesome

--- web-app/components/Header.tsx
+++ web-app/components/Header.tsx
@@ -1,4 +1,4 @@
-export default function Example() {
+export default function Header() {
   return (
     <div className="bg-white shadow">
       <div className="mx-auto">
@@ -9,6 +9,13 @@ export default function Example() {
               <p className="ml-2 text-sm font-medium text-gray-500 hover:text-gray-700">NEAR University Certificate Browser</p>
             </div>
           </a>
+          <div className="relative">
+            <a href="https://jobs.near.university/">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                <p className="ml-2 text-sm font-medium text-gray-500 hover:text-gray-700">Jobs</p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
