2d4feb10c6fbb3398c92bc2a74a2b79ae4342cb8
cagatay soyer <cagataysoyer@gmail.com>
2022-02-14 21:26:25 +0300

    add: designs, and improve the switch functionality

--- web-app/helpers/certificate-designs.ts
+++ web-app/helpers/certificate-designs.ts
@@ -30,8 +30,13 @@ export async function populateDeveloperCert(canvas: Canvas, details: any) {
   console.log('populateDeveloperCert', { details });
   const { tokenId, date, programName, accountName, expiration, programDescription, instructor, programCode } = details;
 
-  const programDescription1 = programDescription.split(',')[0];
-  const programDescription2 = programDescription.split(',')[1];
+  // splitting the sentence into 3 so that we
+  // can properly display them on the cert
+  // needs a better solution than this!
+  const programDescription4 = programDescription.split('program ')[1];
+  const programDescription1 = programDescription.split('by')[0];
+  const programDescription2 = programDescription4.split('on ')[0];
+  const programDescription3 = programDescription4.split('contracts ')[1];
 
   const gray = '#757575';
   const black = '#000000';
@@ -43,17 +48,42 @@ export async function populateDeveloperCert(canvas: Canvas, details: any) {
   // Load and draw the background image first
   // Background images must be in SVG format
   const certificateBackgroundNcdImage = './public/certificate-backgrounds/NCD_certificate.svg';
-  const certificateBackgroundNcaImage = './public/certificate-backgrounds/NCD_background.svg';
+  const certificateBackgroundNcaImage = './public/certificate-backgrounds/NCA_certificate.svg';
+  const certificateBackgroundNceImage = './public/certificate-backgrounds/NCE_certificate.svg';
+  const certificateBackgroundNciImage = './public/certificate-backgrounds/NCI_certificate.svg';
+  const certificateBackgroundNcarImage = './public/certificate-backgrounds/NCAR_certificate.svg';
+  const certificateBackgroundNcsImage = './public/certificate-backgrounds/NCS_certificate.svg';
+  const certificateBackgroundNcuxImage = './public/certificate-backgrounds/NCUX_certificate.svg';
 
   let image;
 
   switch (programCode) {
-    case 'TR101': // programCode needs to change in a meaningfull way.
+    case 'TR101': // programCode needs to change in a meaningful way.
       image = await loadImage(certificateBackgroundNcdImage);
       console.log({ image });
       break;
     case 'TR102':
       image = await loadImage(certificateBackgroundNcaImage);
+      console.log({ image });
+      break;
+    case 'TR103':
+      image = await loadImage(certificateBackgroundNceImage);
+      console.log({ image });
+      break;
+    case 'TR104':
+      image = await loadImage(certificateBackgroundNciImage);
+      console.log({ image });
+      break;
+    case 'TR105':
+      image = await loadImage(certificateBackgroundNcarImage);
+      console.log({ image });
+      break;
+    case 'TR106':
+      image = await loadImage(certificateBackgroundNcsImage);
+      console.log({ image });
+      break;
+    case 'TR107':
+      image = await loadImage(certificateBackgroundNcuxImage);
       console.log({ image });
       break;
     default:
@@ -67,6 +97,7 @@ export async function populateDeveloperCert(canvas: Canvas, details: any) {
   addText(canvas, programName, programFont, black, 240, 670);
   addText(canvas, programDescription1, dateFont, gray, 65, 450);
   addText(canvas, programDescription2, dateFont, gray, 65, 500);
+  addText(canvas, programDescription3, dateFont, gray, 65, 550);
   addText(canvas, instructor, dateFont, black, 200, 800);
   addText(canvas, date, dateFont, black, 830, 803);
   addText(canvas, expiration, dateFont, black, 830, 860);
