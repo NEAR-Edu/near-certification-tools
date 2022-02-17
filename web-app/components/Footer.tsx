f54e458405396126e802662c98c99960c6069059
ozan isgor <ozanisgor11@gmail.com>
2022-02-17 14:54:11 +0300

    update: order footer content

--- web-app/components/Footer.tsx
+++ web-app/components/Footer.tsx
@@ -6,6 +6,14 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 shadow-[1px_1px_10px_1px_rgba(0,0,0,0.2)]">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 pb-12 md:pb-0">
+          <div className="mt-8 md:mt-0 md:order-1">
+            <a href="https://NEAR.University" target="_blank" rel="noopener noreferrer">
+              <img alt="NEAR University logo" width="200px" src="/near_university_logo.svg" />{' '}
+            </a>
+          </div>
+        </div>
+        <FooterSocials />
         <div className="flex justify-center space-x-6">
           {navigation.map((item) => (
             <div key={item.name} className="px-5 py-2">
@@ -15,14 +23,6 @@ export default function Footer() {
             </div>
           ))}
         </div>
-        <div className="flex justify-center space-x-6 pb-12 md:pb-0">
-          <div className="mt-8 md:mt-0 md:order-1">
-            <a href="https://NEAR.University" target="_blank" rel="noopener noreferrer">
-              <img alt="NEAR University logo" width="200px" src="/near_university_logo.svg" />{' '}
-            </a>
-          </div>
-        </div>
-        <FooterSocials />
       </div>
     </footer>
   );
