7724acec7254c703caceb2c4c4d7d512b95accd6
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2022-02-18 20:59:09 +0300

    WIP: make the title and description dynamic according to certificate

--- web-app/pages/certificate/[tokenId].tsx
+++ web-app/pages/certificate/[tokenId].tsx
@@ -10,7 +10,10 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import { getNearAccountWithoutAccountIdOrKeyStoreForBackend } from '../../helpers/near';
 import { baseUrl } from '../../helpers/strings';
+import { formatDate } from '../../helpers/time';
+import { getNftContract, NFT } from '../api/mint-cert';
 
 function buildTwitterUrl(pngUrl: string) {
   // https://stevenwestmoreland.com/2018/07/creating-social-sharing-links-without-javascript.html
@@ -42,62 +45,59 @@ function buildLinkedInUrl(pngUrl: string) {
 // eslint-disable-next-line max-lines-per-function
 const Certificate: NextPage = (...props) => {
   const router = useRouter();
-  const { tokenId } = router.query; // https://nextjs.org/docs/routing/dynamic-routes  
-  const data = tokenId;
-  const pngUrl = `${baseUrl}/api/cert/${data}.png`;
-  // eslint-disable-next-line react/destructuring-assignment
-  console.log('************************', { data });
+  const { tokenId } = router.query; // https://nextjs.org/docs/routing/dynamic-routes
+  const tokenUrl = tokenId;
+  const metaProps = props[0];
+  const metadataResult = Object.values(metaProps);
+  const pngUrl = `${baseUrl}/api/cert/${tokenUrl}.png`;
   return (
-    <Layout>
+    <>
       <Head>
-        <meta property="og:url" content={`/api/cert/${data}`} />
+        <meta property="og:url" content={`/api/cert/${tokenUrl}`} />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="I got certified with NEAR university" />
-        <meta property="og:description" content="View NEAR University certificates of any .near account" />
-        <meta property="og:image" content={`/api/cert/${data}.png`} />
+        <meta property="og:title" content={`${metadataResult[0]}`} />
+        <meta property="og:description" content={`${metadataResult[1]}`} />
+        <meta property="og:image" content={`/api/cert/${tokenUrl}.png`} />
         <meta property="twitter:site" content="@NEARProtocol" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="/" />
-        <meta property="twitter:url" content={`/certificate/${data}`} />
-        <meta name="twitter:title" content="I got certified with NEAR university" />
-        <meta name="twitter:description" content="View NEAR University certificates of any .near account" />
-        <meta name="twitter:image" content={`/api/cert/${data}.png`} />
+        <meta property="twitter:url" content={`/certificate/${tokenUrl}`} />
+        <meta name="twitter:title" content={`${metadataResult[0]}`} />
+        <meta name="twitter:description" content={`${metadataResult[1]}`} />
+        <meta name="twitter:image" content={`/api/cert/${tokenUrl}.png`} />
       </Head>
-      <a href={`/api/cert/${tokenId}.svg`}>
-        <img src={pngUrl} alt={`certificate ${tokenId}`} />
-      </a>
-      <div className="text-sm mt-2 ml-2">
-        Share:{' '}
-        <a href={buildTwitterUrl(pngUrl)} target="_blank" rel="noreferrer">
-          <i className="fab fa-twitter-square not-italic text-sky-700 p-1" />
+      <Layout>
+        <a href={`/api/cert/${tokenId}.svg`}>
+          <img src={pngUrl} alt={`certificate ${tokenId}`} />
         </a>
-        <a href={buildLinkedInUrl(pngUrl)} target="_blank" rel="noreferrer">
-          <i className="fab fa-linkedin-in not-italic text-sky-700 p-1" />
-        </a>
-      </div>
-    </Layout>
+        <div className="text-sm mt-2 ml-2">
+          Share:{' '}
+          <a href={buildTwitterUrl(pngUrl)} target="_blank" rel="noreferrer">
+            <i className="fab fa-twitter-square not-italic text-sky-700 p-1" />
+          </a>
+          <a href={buildLinkedInUrl(pngUrl)} target="_blank" rel="noreferrer">
+            <i className="fab fa-linkedin-in not-italic text-sky-700 p-1" />
+          </a>
+        </div>
+      </Layout>
+    </>
   );
 };
 export async function getServerSideProps(context: any) {
   const { tokenId } = context.query;
-  console.log('---------------------------', { tokenId });
-  const url = `${baseUrl}/api/cert/${tokenId}`;
-  console.log('++++++++++++++++++++++++++++++++++++++++', { url });
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'image/png',
-    },
-  };
-  console.log('++++++++++++++++++++++++++++++++++++++++', { requestOptions });
-  const results = tokenId;
-  const fetchRes = await fetch(url, requestOptions);
-  console.log('+++++++++++++++++++++++++++++++++++++++++', { fetchRes });
-  // const resJson = await fetchRes.json();
-  // console.log('+++++++++++++++++++++', resJson);
+  const account = await getNearAccountWithoutAccountIdOrKeyStoreForBackend();
+  const contract = getNftContract(account);
+  const response = await (contract as NFT).nft_token({ token_id: tokenId });
+  const { metadata } = response;
+
+  const programName = metadata.title;
+  const programDescription = metadata.description;
+  const date = formatDate(metadata.issued_at);
   return {
     props: {
-      children: results,
+      title: programName,
+      description: programDescription,
+      date,
     },
   };
 }
