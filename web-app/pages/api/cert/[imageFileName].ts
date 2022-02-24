cfd2801a8e7a577e47f131e4eb0775827a65f27b
Ryan Walsh <rcwalsh@gmail.com>
2022-02-24 15:04:22 -0500

    adjusting important comments

--- web-app/pages/api/cert/[imageFileName].ts
+++ web-app/pages/api/cert/[imageFileName].ts
@@ -50,18 +50,33 @@ async function generateImage(canvasType: CanvasTypeDef, bufferType: BufferTypeDe
   return buffer;
 }
 
+/**
+ * // TODO: See important comment at getExpiration.
+ * This query uses Common Table Expressions (CTE) to execute two separate queries conditionally, the second query being executed if the first query doesn't return any result.
+ * https://www.postgresql.org/docs/9.1/queries-with.html
+ * https://stackoverflow.com/a/68684814/10684149
+ * // TODO: Should we switch to the "Simpler for a single returned row" approach at https://stackoverflow.com/a/68684814/470749?
+ *
+ * The first query (long_period_of_inactivity) checks whether the account has a period where it hasn't been active for >180 consecutive days after the issue date.
+ * The second query (most_recent_activity) is run if no >180-day-inactivity period is found and returns the most recent activity date AND diff in days between "now"
+ *  (render date) and account's last activity date.
+ *
+ * 1 day = 60 sec * 60 min * 24 hr = 86400 sec
+ * included_in_block_timestamp uses nanoseconds so is divided by 1B when using TO_TIMESTAMP (which expects seconds).
+ */
 // eslint-disable-next-line max-lines-per-function
 function getRawQuery(accountName: string, issuedAtUnixNano: number) {
+  console.log({ accountName, issuedAtUnixNano, expirationDays });
   return Prisma.sql`
     WITH long_period_of_inactivity AS (
       SELECT 
       moment,
       diff_to_previous_activity,
-      CAST(NULL AS int) AS diff_from_last_activity_to_render_date, /*  to match column numbers in both queries  */
+      CAST(NULL AS int) AS diff_from_last_activity_to_render_date, /* The columns returned by each query must match */
       true AS has_long_period_of_inactivity
       FROM (
         SELECT *,
-          ((EXTRACT(epoch FROM moment) - EXTRACT(epoch FROM lag(moment) over (ORDER BY moment))) / 86400)::int /* 1 day = 60sec * 60min * 24h = 86400 sec*/
+          ((EXTRACT(epoch FROM moment) - EXTRACT(epoch FROM lag(moment) over (ORDER BY moment))) / (60 * 60 * 24))::int
           AS diff_to_previous_activity
         FROM (
           SELECT TO_TIMESTAMP(R."included_in_block_timestamp"/1000000000) as moment
@@ -77,8 +92,8 @@ function getRawQuery(accountName: string, issuedAtUnixNano: number) {
       ), most_recent_activity AS (
       SELECT
       moment, 
-      CAST(NULL AS int) AS diff_to_previous_activity, /*  to match column numbers in both queries  */
-      ((EXTRACT(epoch FROM CURRENT_TIMESTAMP) - EXTRACT(epoch FROM moment)) / 86400)::int 
+      CAST(NULL AS int) AS diff_to_previous_activity, /* The columns returned by each query must match */
+      ((EXTRACT(epoch FROM CURRENT_TIMESTAMP) - EXTRACT(epoch FROM moment)) / (60 * 60 * 24))::int 
 		  AS diff_from_last_activity_to_render_date,
       false AS has_long_period_of_inactivity
       FROM (
@@ -97,20 +112,21 @@ function getRawQuery(accountName: string, issuedAtUnixNano: number) {
     TABLE most_recent_activity`;
 }
 
+/**
+ * // TODO: We need to first get much clearer on the definition of expiration and create some test data for all the various scenarios. Then we can comment a summary here and fix
+ * the implementation of this function and getRawQuery.
+ *
+ * If at least one >180-day period of inactivity after issueDate exists (has_long_period_of_inactivity === true):
+ *   Expiration date = the beginning of the *first* such period + 180 days.
+ * Otherwise, calculate diff (in days) between now (render date) and date of last activity.
+ *   If this value (diff_from_last_activity_to_render_date) is >180;
+ *     Certificate is expired. Expiration date = last activity + 180 days
+ *   If this value (diff_from_last_activity_to_render_date) is <180;
+ *     Certificate hasn't expired yet. Expiration date = last activity + 180 days
+ */
 async function getExpiration(accountName: string, issuedAt: string): Promise<string> {
   // Pulls from the public indexer. https://github.com/near/near-indexer-for-explorer#shared-public-access
-  /**
-   * This query uses Common Table Expressions(CTE) to execute two separate queries conditionally;
-   * the second query being executed if first query doesn't return any result.
-   * https://www.postgresql.org/docs/9.1/queries-with.html
-   * https://stackoverflow.com/a/68684814/10684149
-   */
-  /**
-   * First query checks If the account has a period where it hasn't been active for 180 days straight after the issue date (exluding the render date)
-   * Second query is run if no 180-day-inactivity period is found and returns most recent activity date
-   * AND amount of days between account's last activity date - render date of certificate
-   */
-  const issuedAtUnixNano = dayjs(issuedAt).unix() * 1_000_000_000; // TODO: See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER and add a comment here and in JSDoc for functions that have a `number` argument for dates. Explain why it's safe to use floating point `number` type (if it is), or switch to a better approach and explain it.
+  const issuedAtUnixNano = dayjs(issuedAt).unix() * 1000000000; // TODO: See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER and add a comment here and in JSDoc for functions that have a `number` argument for dates. Explain why it's safe to use floating point `number` type (if it is), or switch to a better approach and explain it.
   console.log({ issuedAt, issuedAtUnixNano, accountName });
   const rawQuery = getRawQuery(accountName, issuedAtUnixNano);
   // https://www.prisma.io/docs/concepts/components/prisma-client/raw-database-access#queryraw
@@ -118,33 +134,13 @@ async function getExpiration(accountName: string, issuedAt: string): Promise<str
 
   console.log('getExpiration query result', { result });
 
-  /**
-   * If the account doesn't have a period where it hasn't been active for 180 days straight after the issue date:
-   * Days between last activity and render date is checked:
-   * If this value (diff_from_last_activity_to_render_date) is  >180;
-   * -- Certificate is expired. Expiration date = last activity + 180 days
-   * If this value (diff_from_last_activity_to_render_date) is <180;
-   * -- Certificate hasn't expired yet. Expiration date = last activity + 180 days
-   * Otherwise, if >180-day period of inactivity exist (has_long_period_of_inactivity === true) after issueDate,
-   * -- Expiration date = the beginning of the *first* such period + 180 days.
-   */
   const moment = dayjs(result[0].moment);
 
   if (result[0].has_long_period_of_inactivity) {
-    /**
-     * >180-day period of inactivity exists. Can be anything over 180.
-     * moment is the end date of such period.
-     * Extract 180 from inactivity period to get the exact days betwen moment and actual expiration date.
-     */
-    const daysToMomentOfExpiration = result[0].diff_to_previous_activity - expirationDays;
-
-    /**
-     * Subtract daysToMomentOfExpiration from moment to get the specific date of expiration.
-     * This subtraction equals to (start of inactivity period + 180 days)
-     */
-    return formatDate(moment.subtract(daysToMomentOfExpiration, 'days'));
-  } else {
     return formatDate(moment.add(expirationDays, 'days'));
+  } else {
+    const daysToMomentOfExpiration = result[0].diff_to_previous_activity - expirationDays;
+    return formatDate(moment.subtract(daysToMomentOfExpiration, 'days'));
   }
 }
 
@@ -165,7 +161,6 @@ async function fetchCertificateDetails(tokenId: string) {
       let expiration = null; // The UI (see `generateImage`) will need to gracefully handle this case when indexer service is unavailable.
       try {
         expiration = await getExpiration(accountName, metadata.issued_at);
-        // E.g.: expiration: { expirationDate: '2022-08-16', isExpired: false }
       } catch (error) {
         console.error('Perhaps a certificate for the original_recipient_id could not be found or the public indexer query timed out.', error);
       }
