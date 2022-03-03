3612f5fef65320617b2d538b9301526d7be546e7
Esin Adsoy <esin.adsoy@gmail.com>
2022-03-03 19:42:14 +0300

    add json data with one account and change test script

--- web-app/__tests__/test-query.test.ts
+++ web-app/__tests__/test-query.test.ts
@@ -2,39 +2,39 @@ import { PrismaClient } from '@prisma/client';
 import { getRawQuery, getExpiration } from '../prisma/test-setup/cert-query-functions';
 
 const prisma = new PrismaClient();
-beforeAll(async () => {
-  // create receipts
-  await prisma.receipts.createMany({
-    data: [
-      {
-        receipt_id: 'receiptidofJaneDoewith45charsxxxxxxxxxxxxxxx',
-        included_in_block_timestamp: 1646224546000000000,
-      },
-      {
-        receipt_id: 'receiptidofJohnDoewith45charsyyyyyyyyyyyyyyy',
-        included_in_block_timestamp: 1645788799000000000,
-      },
-    ],
-  });
+// beforeAll(async () => {
+//   // create receipts
+//   await prisma.receipts.createMany({
+//     data: [
+//       {
+//         receipt_id: 'receiptidofJaneDoewith45charsxxxxxxxxxxxxxxx',
+//         included_in_block_timestamp: 1646224546000000000,
+//       },
+//       {
+//         receipt_id: 'receiptidofJohnDoewith45charsyyyyyyyyyyyyyyy',
+//         included_in_block_timestamp: 1645788799000000000,
+//       },
+//     ],
+//   });
 
-  // create action_receipts
-  await prisma.action_receipts.createMany({
-    data: [
-      {
-        receipt_id: 'receiptidofJaneDoewith45charsxxxxxxxxxxxxxxx',
-        signer_account_id: 'janedoe.testnet',
-      },
-      {
-        receipt_id: 'receiptidofJohnDoewith45charsyyyyyyyyyyyyyyy',
-        signer_account_id: 'johndoe.testnet',
-      },
-    ],
-  });
+//   // create action_receipts
+//   await prisma.action_receipts.createMany({
+//     data: [
+//       {
+//         receipt_id: 'receiptidofJaneDoewith45charsxxxxxxxxxxxxxxx',
+//         signer_account_id: 'janedoe.testnet',
+//       },
+//       {
+//         receipt_id: 'receiptidofJohnDoewith45charsyyyyyyyyyyyyyyy',
+//         signer_account_id: 'johndoe.testnet',
+//       },
+//     ],
+//   });
 
-  console.log('✨ 2 action_receipts successfully created!');
+//   console.log('✨ 2 action_receipts successfully created!');
 
-  console.log('✨ 2 receipts successfully created!');
-});
+//   console.log('✨ 2 receipts successfully created!');
+// });
 
 afterAll(async () => {
   const deleteReceipt = prisma.action_receipts.deleteMany();
