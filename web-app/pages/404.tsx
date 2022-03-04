de236e7f667c51f3b303045dc027c7dedb5a4b15
cagatay soyer <cagataysoyer@gmail.com>
2022-03-04 11:05:25 +0300

    update: rename function and text

--- web-app/pages/404.tsx
+++ web-app/pages/404.tsx
@@ -1,4 +1,4 @@
-export default function Example() {
+export default function handle404() {
   return (
     <div className="bg-white px-4 py-16 sm:px-6 sm:py-24 sm:h-screen md:grid md:place-content-center lg:px-8">
       <div className="max-w-max mx-auto">
@@ -7,7 +7,7 @@ export default function Example() {
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
               <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Page not found</h1>
-              <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              <p className="mt-1 text-base text-gray-500">There is no page at this address.</p>
             </div>
             <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
               <a
