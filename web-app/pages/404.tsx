ac93a34aa96b61b62431771a26b9b9dbe9094131
cagatay soyer <cagataysoyer@gmail.com>
2022-03-08 18:34:31 +0300

    fix: rename 404 text

--- web-app/pages/404.tsx
+++ web-app/pages/404.tsx
@@ -3,7 +3,7 @@ export default function ErrorPage404() {
     <div className="bg-white px-4 py-16 sm:px-6 sm:py-24 sm:h-screen md:grid md:place-content-center lg:px-8">
       <div className="max-w-max mx-auto">
         <main className="sm:flex">
-          <p className="text-4xl font-extrabold text-purple-600 sm:text-5xl">404</p>
+          <p className="text-4xl font-extrabold text-purple-600 sm:text-5xl">ERROR</p>
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
               <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Page not found</h1>
