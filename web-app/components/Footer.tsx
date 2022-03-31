31bf15a517d09237755b40d98028ea1812e98758
Ryan Walsh <rcwalsh@gmail.com>
2022-03-31 14:14:43 -0400

    added feedback link to footer

--- web-app/components/Footer.tsx
+++ web-app/components/Footer.tsx
@@ -8,8 +8,11 @@ export default function Footer() {
           <img className="min-w-full" alt="NEAR University logo" width="200px" src="/near_university_logo.svg" />
         </a>
         <FooterSocials />
-        <a href="https://jobs.near.university/" target="_blank" rel="noopener noreferrer" className="text-base font-medium text-gray-500 hover:text-gray-900 py-5">
+        <a href="https://jobs.near.university/" target="_blank" rel="noopener noreferrer" className="text-base font-medium text-gray-500 hover:text-gray-900 pr-3">
           Jobs
+        </a>
+        <a href="https://forms.gle/EKpNXNGuwLvuribn8" target="_blank" rel="noopener noreferrer" className="text-base font-medium text-gray-500 hover:text-gray-900 pr-3">
+          Feedback
         </a>
       </div>
     </footer>
