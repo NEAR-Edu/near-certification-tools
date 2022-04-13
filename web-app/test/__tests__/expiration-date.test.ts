a72b99e1cd5e319be4ef33663b1fab89420800f2
Esin Adsoy <esin.adsoy@gmail.com>
2022-04-14 00:43:34 +0300

    refactor comments, assign issueDate at beginning of every test case and delete test case for john.testnet

--- web-app/test/__tests__/expiration-date.test.ts
+++ web-app/test/__tests__/expiration-date.test.ts
@@ -14,35 +14,27 @@ afterAll(async () => {
   await prisma.$disconnect();
 });
 
-// TODO: Complete cases
-// TODO: NOTE: Does certifying an account register as activity?
-/**
- * Patricia's cert was issued 2022-04-01,  — issueDate
- * she has not had any mainnet activity since then.
- * --
- * her cert is tentatively scheduled to expire 2022-04-01 + 180.
- */
-// TODO: -end
-
 // eslint-disable-next-line max-lines-per-function
 describe('test expiration date functions', () => {
   // eslint-disable-next-line max-lines-per-function
   describe('accounts with 180 day inactivity after issue date', () => {
-    // -- Test Case -1 --
-    // NOTE: SALLY
-    /**
-     * Sally’s certificate was issued_at 2021-03-02T00:00:00+00:00, ==> issueDate
-     * She had no mainnet activitiy for 296 days. ==> >180-days of inactivity
-     * Her last mainnet activity was on 2021-12-23T09:46:39+00:00 ==> moment
-     * and she hasn’t been active since 2021-12-23T09:46:39+00:00.
-     * --
-     * Her cert has expired on
-     * = 2021-03-02 + 180 days ( = 2021-12-23 - (296 - 180)days)
-     * = 2021-08-29
-     */
     describe('account with 180 day inactivity and no frequent activity after issue date of certificate', () => {
+      // -- Test Case 1 --
+      // ACCOUNT: sally.testnet
+      /**
+       * Sally’s certificate was issued_at 2021-03-02T00:00:00+00:00,
+       * She had no mainnet activitiy for 296 days.
+       * Her last mainnet activity was on 2021-12-23T09:46:39+00:00
+       * and she hasn’t been active since 2021-12-23T09:46:39+00:00.
+       * --
+       * Her cert has expired on
+       * = 2021-03-02 + 180 days ( = 2021-12-23 - (296 - 180)days)
+       * = 2021-08-29
+       */
+      const issueDate = convertStringDateToMilliseconds('2021-03-02T00:00:00+00:00');
+
       it('should return query result for sally with first occurence of 180-day inactivity period', async () => {
-        const queryResult = await getRawQueryResult('sally.testnet', convertStringDateToMilliseconds('2021-03-02T00:00:00+00:00'));
+        const queryResult = await getRawQueryResult('sally.testnet', issueDate);
         expect(queryResult).toEqual(
           expect.arrayContaining([
             {
@@ -59,25 +51,27 @@ describe('test expiration date functions', () => {
          * Certificate expired 296 - 180 = 116 days prior to moment.
          * expiration date = 2021-12-23 - 116 = 2021-03-02 + 180 days = 2022-08-29
          */
-        await expect(getExpiration('sally.testnet', convertStringDateToMilliseconds('2021-03-02T00:00:00+00:00'))).resolves.toEqual('2021-08-29');
+        await expect(getExpiration('sally.testnet', issueDate)).resolves.toEqual('2021-08-29');
       });
     });
-    // -- Test Case - 2 --
-    // NOTE: STEVE
-    /**
-     * Steve's cert was issued_at 2021-01-05
-     * He had frequent mainnet activity for a couple of months (2021-03-16T20:08:59+00:00)
-     * but then no mainnet activity for 204 days (i.e. >180-days of inactivity)
-     * and then had some more mainnet activity.
-     * His last mainnet activity was on 2022-03-05T09:46:39+00:00
-     * But none of that activity after his 180+ days of inactivity matters because his certificate should have expired 180 days after the
-     * beginning of the first long period of inactivity (>=180 days).
-     * --
-     * His cert should have an expiration of: 2021-03-16 + 180 = 2021-09-12
-     * Double-check from another angle: Certificate expired 204 - 180 = 24 days prior to moment.
-     * So, expiration date = 2021-10-06 - 24 days = 2021-09-12 = 2021-03-16 + 180 days
-     */
+
     describe('account with 180 day inactivity and frequent activity after issue date of certificate', () => {
+      // -- Test Case 2 --
+      // ACCOUNT: steve.testnet
+      /**
+       * Steve's cert was issued_at 2021-01-05T11:15:09+00:00
+       * He had frequent mainnet activity for a couple of months (2021-03-16T20:08:59+00:00)
+       * but then no mainnet activity for 204 days (i.e. >180-days of inactivity)
+       * and then had some more mainnet activity.
+       * His last mainnet activity was on 2022-03-05T09:46:39+00:00
+       * But none of that activity after his 180+ days of inactivity matters because his certificate should have expired 180 days after the
+       * beginning of the first long period of inactivity (>=180 days).
+       * --
+       * His cert should have an expiration of: 2021-03-16 + 180 = 2021-09-12
+       * Double-check from another angle: Certificate expired 204 - 180 = 24 days prior to moment.
+       * So, expiration date = 2021-10-06 - 24 days = 2021-09-12 = 2021-03-16 + 180 days
+       */
+
       const issueDate = convertStringDateToMilliseconds('2021-01-05T11:15:09+00:00');
 
       it('should return query result for Steve', async () => {
@@ -85,7 +79,7 @@ describe('test expiration date functions', () => {
         expect(queryResult).toEqual(
           expect.arrayContaining([
             {
-              moment: '2021-10-06T22:10:05+00:00',
+              moment: '2021-10-06T12:10:05+00:00',
               diff_to_previous_activity: 204,
               has_long_period_of_inactivity: true,
             },
@@ -102,49 +96,19 @@ describe('test expiration date functions', () => {
   // eslint-disable-next-line max-lines-per-function
   describe('accounts with frequent activity after issue date', () => {
     describe('account with frequent activity after issue date', () => {
-      // -- Test Case - 3 --
-      // NOTE: JOHN
+      // -- Test Case 3 --
+      // ACCOUNT: rebecca.testnet
       /**
-       * John's certificate was issued_at 2022-03-02T00:00:00+00:00, ==> issueDate
-       * He has frequent mainnet activity since the issue date
-       * His last mainnet activity was on 2021-04-05T09:46:39+00:00 ==> moment
+       * Rebecca's cert was issued 2021-08-03T00:00:00+00:00
+       * She has continued to have mainnet activity every couple of days through 2022-04-07T16:25:59+00:00
        * --
-       * His cert is tentatively scheduled to expire 2022-04-05 + 180 days
-       * = 2022-10-02
+       * Her cert is tentatively scheduled to expire 2022-04-07T16:25:59+00:00 + 180 days.
        */
-      it('should return query result for John', async () => {
-        const queryResult = await getRawQueryResult('john.testnet', convertStringDateToMilliseconds('2022-03-02T00:00:00+00:00'));
-        expect(queryResult).toEqual(
-          expect.arrayContaining([
-            {
-              moment: '2022-04-05T17:24:06+00:00',
-              diff_to_previous_activity: null,
-              has_long_period_of_inactivity: false,
-            },
-          ]),
-        );
-      });
 
-      it('should return expiration date as last activity date + 180 for account with no 180-day inactivity period', async () => {
-        /**
-         * last Activity: 2022-04-05
-         * expiration date = 2022-04-05 + 180 days = 2022-10-02
-         */
-        await expect(getExpiration('john.testnet', convertStringDateToMilliseconds('2022-03-02T00:00:00+00:00'))).resolves.toEqual('2022-10-02');
-      });
-    });
+      const issueDate = convertStringDateToMilliseconds('2021-08-03T00:00:00+00:00');
 
-    describe('account with frequent activity after issue date', () => {
-      // -- Test Case - 4 --
-      // NOTE: REBECCA
-      /**
-       * Rebecca's cert was issued 2021-08-03
-       * he has continued to have mainnet activity every couple of days through 05-04-2022
-       * --
-       * His cert is tentatively scheduled to expire  05-04-2022  + 180.
-       */
       it('should return query result for Rebecca', async () => {
-        const queryResult = await getRawQueryResult('rebecca.testnet', convertStringDateToMilliseconds('2021-08-03T00:00:00+00:00'));
+        const queryResult = await getRawQueryResult('rebecca.testnet', issueDate);
         expect(queryResult).toEqual(
           expect.arrayContaining([
             {
@@ -158,68 +122,41 @@ describe('test expiration date functions', () => {
 
       it('should return expiration date as last activity date + 180 for account with no 180-day inactivity period', async () => {
         /**
-         * last Activity: 2022-04-07
-         * expiration date = 2022-04-07 + 180 days = 2022-10-04
+         * last Activity: 2022-04-07T16:25:59+00:00
+         * expiration date = 2022-04-07T16:25:59+00:00 + 180 days = 2022-10-04
          */
-        await expect(getExpiration('rebecca.testnet', convertStringDateToMilliseconds('2021-08-03T00:00:00+00:00'))).resolves.toEqual('2022-10-04');
+        await expect(getExpiration('rebecca.testnet', issueDate)).resolves.toEqual('2022-10-04');
       });
-    });
-
-    describe('', () => {
-      // -- Test Case - 7 --
-      // NOTE: WILLIAM
-      /**
-       * William's cert was issued 2022-04-06T01:00:00+00:00
-       * he has continued to have mainnet activity every couple of minutes through 06-04-2022T00:10:00+00:00
-       * --
-       * His cert is tentatively scheduled to expire  06-04-2022  + 180.
-       */
-      // it('should return query result for john', async () => {
-      //   const queryResult = await getRawQueryResult('william.testnet', convertStringDateToMilliseconds('2022-03-02T00:00:00+00:00'));
-      //   expect(queryResult).toEqual(
-      //     expect.arrayContaining([
-      //       {
-      //         moment: '2022-04-05T17:24:06+00:00',
-      //         diff_to_previous_activity: null,
-      //         has_long_period_of_inactivity: false,
-      //       },
-      //     ]),
-      //   );
-      // });
-      // it('should return expiration date as last activity date + 180 for account with no 180-day inactivity period', async () => {
-      //   await expect(getExpiration('william.testnet', convertStringDateToMilliseconds(dayjs().toISOString()))).resolves.toEqual(formatDate(dayjs()));
-      // });
     });
   });
 
   // eslint-disable-next-line max-lines-per-function
   describe('accounts with multiple 180 day inactivity after issue date of certificate', () => {
-    // -- Test Case - 5 --
-    // NOTE: BOB
-    /**
-     * Bob's certificate was issued_at 2018-10-01T00:00:00+00:00, ==> issueDate
-     * he has not had any mainnet activity for 365 days => >180-days of inactivity
-     * then had frequent mainnet activity for a couple of years (through 2021-05-07T13:20:37+00:00)
-     * then again no mainnet activity for 184 days => >180-days of inactivity
-     * then, had frequent mainnet activity for a couple of months (through 2022-03-04T13:20:37+00:00)
-     * His last mainnet activity was on 2022-03-04T13:20:37+00:00
-     * --
-     * His cert has expired on
-     * = 2018-10-01 + 180 days (= 2019-10-01 - (365 - 180)days)
-     * = 2019-03-30, during the FIRST inactivity period
-     */
     describe('account with multiple 180 day inactivity and frequent activity after issue date of certificate', () => {
+      // -- Test Case 4 --
+      // ACCOUNT: bob.testnet
       /**
-       * Certificate expired 365 - 180 = 185 days prior to moment (2019-10-01).
-       * expiration date = 2019-10-01 - 185 = 2018-10-01 + 180 days = 2019-03-30
+       * Bob's certificate was issued_at 2018-10-01T00:00:00+00:00, ==> issueDate
+       * he has not had any mainnet activity for 365 days => >180-days of inactivity
+       * then had frequent mainnet activity for a couple of years (through 2021-05-07T13:20:37+00:00)
+       * then again no mainnet activity for 184 days => >180-days of inactivity
+       * then, had frequent mainnet activity for a couple of months (through 2022-03-04T13:20:37+00:00)
+       * His last mainnet activity was on 2022-03-04T13:20:37+00:00
+       * --
+       * His cert has expired on
+       * = 2018-10-01 + 180 days (= 2019-10-01 - (365 - 180)days)
+       * = 2019-03-30, during the FIRST inactivity period
        */
+
+      const issueDate = convertStringDateToMilliseconds('2018-10-01T00:00:00+00:00');
+
       it('should return query result for bob when 180-days inactivity is present and moment should be the most recent date of such period', async () => {
-        const queryResult = await getRawQueryResult('bob.testnet', convertStringDateToMilliseconds('2018-10-01T00:00:00+00:00'));
+        const queryResult = await getRawQueryResult('bob.testnet', issueDate);
 
         expect(queryResult).toEqual(
           expect.arrayContaining([
             {
-              moment: '2019-10-01T13:20:37+00:00',
+              moment: '2019-10-01T00:00:00+00:00',
               diff_to_previous_activity: 365,
               has_long_period_of_inactivity: true,
             },
@@ -228,28 +165,27 @@ describe('test expiration date functions', () => {
       });
 
       it('should return expiration date for Bob as last activity date - (diff_to_previous_activity - 180)', async () => {
-        // expiration date = last activity + 180 = 2022-03-04 + 180 = 2022-09-05
-        await expect(getExpiration('bob.testnet', convertStringDateToMilliseconds('2018-10-01T00:00:00+00:00'))).resolves.toEqual('2019-03-30');
+        await expect(getExpiration('bob.testnet', issueDate)).resolves.toEqual('2019-03-30');
       });
     });
-    // -- Test Case - 6 --
-    // NOTE: ALICE
-    /**
-     * Alice's cert was issued 2019-08-03
-     * she has not had any mainnet activity for 214 days
-     * then again no mainnet activity for 190 days
-     * then again no mainnet activity for 182 days
-     * and has not had any mainnet activity since then.
-     * --
-     * Her certficate expired on
-     */
+
     describe('account with multiple 180 day inactivity and frequent activity after issue date of certificate', () => {
+      // -- Test Case 5 --
+      // ACCOUNT: alice.testnet
       /**
-       * Certificate expired 214 - 180 = 34 days prior to moment (2020-03-04).
-       * expiration date = 2020-03-04 - 34 = 2019-08-03 + 180 days = 2020-01-30
+       * Alice's cert was issued 2019-08-03
+       * she has not had any mainnet activity for 214 days
+       * then again no mainnet activity for 190 days
+       * then again no mainnet activity for 182 days
+       * and has not had any mainnet activity since then.
+       * --
+       * Her certficate expired on
        */
+
+      const issueDate = convertStringDateToMilliseconds('2019-08-03T00:00:00+00:00');
+
       it('should return query result for Alice when 180-days inactivity is present and moment should be the most recent date of such period', async () => {
-        const queryResult = await getRawQueryResult('alice.testnet', convertStringDateToMilliseconds('2019-08-03T00:00:00+00:00'));
+        const queryResult = await getRawQueryResult('alice.testnet', issueDate);
 
         expect(queryResult).toEqual(
           expect.arrayContaining([
@@ -264,7 +200,7 @@ describe('test expiration date functions', () => {
 
       it('should return expiration date for Alice as last activity date - (diff_to_previous_activity - 180)', async () => {
         // expiration date = 2020-03-04 - 34 = 2019-08-03 + 180 days = 2020-01-30
-        await expect(getExpiration('alice.testnet', convertStringDateToMilliseconds('2019-08-03T00:00:00+00:00'))).resolves.toEqual('2020-01-30');
+        await expect(getExpiration('alice.testnet', issueDate)).resolves.toEqual('2020-01-30');
       });
     });
   });
