a2169848a19e92943453c4f01816e6be3bcd0fdb
Ryan Walsh <rcwalsh@gmail.com>
2022-04-16 09:30:29 -0400

    starting to create endpoint for invalidateAllCertsForAccount (see #49)

--- web-app/helpers/near.ts
+++ web-app/helpers/near.ts
@@ -1,12 +1,25 @@
 // Inspired by https://github.com/mehtaphysical/near-js/blob/f1d12884f80cb556472a8109e822c25fdff3c077/packages/next-template-near/services/near.js
-import { KeyPair, Account, connect, ConnectConfig } from 'near-api-js'; // https://github.com/near/near-api-js/blob/master/examples/quick-reference.md
+import type { NextApiResponse } from 'next';
+import { KeyPair, Account, connect, ConnectConfig, Contract } from 'near-api-js'; // https://github.com/near/near-api-js/blob/master/examples/quick-reference.md
 import { BrowserLocalStorageKeyStore, InMemoryKeyStore, KeyStore } from 'near-api-js/lib/key_stores';
+import { IncomingHttpHeaders } from 'http';
 
+const privateKey = process.env.ISSUING_AUTHORITY_PRIVATE_KEY || '';
+export const apiKey = process.env.API_KEY || '';
+// public vars:
+const certificateContractName = process.env.NEXT_PUBLIC_CERTIFICATE_CONTRACT_NAME || 'example-contract.testnet';
+export const issuingAuthorityAccountId = process.env.NEXT_PUBLIC_ISSUING_AUTHORITY_ACCOUNT_ID || 'example-authority.testnet';
+export const gas = process.env.NEXT_PUBLIC_GAS || 300000000000000;
 export const networkId = process.env.NEXT_PUBLIC_NEAR_NETWORK_ID || 'testnet';
 const nodeUrl = process.env.NEXT_PUBLIC_NEAR_NODE_URL || 'https://rpc.testnet.near.org';
 const walletUrl = process.env.NEXT_PUBLIC_NEAR_WALLET_URL || 'https://wallet.testnet.near.org';
 const helperUrl = process.env.NEXT_PUBLIC_NEAR_HELPER_URL || 'https://helper.testnet.near.org';
 // const explorerUrl = process.env.NEXT_PUBLIC_NEAR_EXPLORER_URL || 'https://explorer.testnet.near.org';
+console.log('public env vars', { certificateContractName, issuingAuthorityAccountId, gas });
+
+export const HTTP_SUCCESS = 200;
+export const HTTP_ERROR = 500;
+const HTTP_UNAUTHORIZED = 401; // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
 
 export type AccountId = string;
 
@@ -24,7 +37,7 @@ export async function getNearConnection(keyStore?: KeyStore) {
   return near;
 }
 
-export async function getNearAccount(accountId: string, privateKey: string) {
+async function getNearAccount(accountId: AccountId) {
   const keyPair = KeyPair.fromString(privateKey);
   const keyStore = new InMemoryKeyStore();
   keyStore.setKey(networkId, accountId, keyPair);
@@ -47,4 +60,37 @@ export async function getNearAccountWithoutAccountIdOrKeyStoreForFrontend() {
   const keyStore = new BrowserLocalStorageKeyStore();
   console.log({ keyStore });
   return getNearAccountWithoutAccountIdOrKeyStore(keyStore);
+}
+
+export type NFT = Contract & {
+  // https://stackoverflow.com/a/41385149/470749
+  nft_mint: (args: any, gas: any, depositAmount: any) => Promise<any>; // TODO Add types
+  cert_invalidate: (args: any, gas: any, depositAmount: any) => Promise<any>;
+  nft_token: (args: any) => Promise<any>;
+  nft_tokens_for_owner: (args: any) => Promise<any>;
+};
+
+export function getNftContractOfAccount(account: Account) {
+  // TODO: Make `account` optional.
+  const contract = new Contract(
+    account, // the account object that is connecting
+    certificateContractName,
+    {
+      viewMethods: ['nft_token', 'nft_tokens_for_owner'], // view methods do not change state but usually return a value
+      changeMethods: ['nft_mint'], // change methods modify state
+    },
+  );
+  return contract;
+}
+
+export async function getNftContract() {
+  const account = await getNearAccount(issuingAuthorityAccountId);
+  const contract = getNftContractOfAccount(account);
+  return contract;
+}
+
+export function rejectAsUnauthorized(res: NextApiResponse<any>, headers: IncomingHttpHeaders) {
+  const errorMsg = 'Unauthorized. Please provide the API key.';
+  console.log({ errorMsg, headers });
+  res.status(HTTP_UNAUTHORIZED).json({ status: 'error', message: errorMsg });
 }
---
+++ web-app/pages/api/invalidate-all-certs-for-account.ts
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { AccountId, apiKey, gas, getNftContract, NFT, HTTP_SUCCESS, HTTP_ERROR, rejectAsUnauthorized } from '../../helpers/near';
+import { depositAmountYoctoNear } from './mint-cert';
+
+const apiKeyHeaderName = 'x-api-key'; // Although the user interface of Integromat shows the capitalization as "X-API-Key", inspecting the actual header reveals that lowercase is used.
+
+async function invalidateAllCertsForAccount(accountId: AccountId) {
+  return;
+  // TODO: This function isn't implemented yet. Is there a way to await each cert_invalidate and then return a result once all have succeeded?
+  const contract = await getNftContract();
+  const tokens = await (contract as NFT).nft_tokens_for_owner(accountId);
+  tokens.forEach((token: string) => {
+    (contract as NFT).cert_invalidate(
+      // https://github.com/near/near-api-js/issues/719
+      token,
+      gas, // attached GAS (optional)
+      depositAmountYoctoNear, // attached deposit in yoctoNEAR (optional).
+    );
+  });
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  // Require that this request is authenticated!
+
+  const { headers } = req; // https://stackoverflow.com/a/63529345/470749
+  if (headers?.[apiKeyHeaderName] === apiKey) {
+    const body = req?.body;
+    console.log({ headers, body });
+    const { accountId } = body; // Eventually we will want to add error-handling / validation.
+    try {
+      const result = await invalidateAllCertsForAccount(accountId);
+      res.status(HTTP_SUCCESS).json({ result });
+    } catch (err) {
+      console.error(err);
+      res.status(HTTP_ERROR).json({ status: 'error', message: 'invalidateAllCertsForAccount failed.' });
+    }
+  } else {
+    rejectAsUnauthorized(res, headers);
+  }
+}
