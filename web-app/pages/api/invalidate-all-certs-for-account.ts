9007c2d027f9e4717b9d58433de0f247b74bde8d
Petar Vujović <petarvujovic98@gmail.com>
2022-04-18 13:37:35 +0200

    [WIP]: await for all invalidations
    Wrap all certificate invalidation calls in a
    Promise.all handler so that they are awaited in
    parallel. Also add some error handling

--- web-app/pages/api/invalidate-all-certs-for-account.ts
+++ web-app/pages/api/invalidate-all-certs-for-account.ts
@@ -6,36 +6,46 @@ import { depositAmountYoctoNear } from './mint-cert';
 const apiKeyHeaderName = 'x-api-key'; // Although the user interface of Integromat shows the capitalization as "X-API-Key", inspecting the actual header reveals that lowercase is used.
 
 async function invalidateAllCertsForAccount(accountId: AccountId) {
-  return;
-  // TODO: This function isn't implemented yet. Is there a way to await each cert_invalidate and then return a result once all have succeeded?
   const contract = await getNftContract();
   const tokens = await (contract as NFT).nft_tokens_for_owner(accountId);
-  tokens.forEach((token: string) => {
-    (contract as NFT).cert_invalidate(
-      // https://github.com/near/near-api-js/issues/719
-      token,
-      gas, // attached GAS (optional)
-      depositAmountYoctoNear, // attached deposit in yoctoNEAR (optional).
+
+  try {
+    await Promise.all(
+      tokens.map((token: string) =>
+        (contract as NFT).cert_invalidate(
+          // https://github.com/near/near-api-js/issues/719
+          token,
+          gas, // attached GAS (optional)
+          depositAmountYoctoNear, // attached deposit in yoctoNEAR (optional).
+        ),
+      ),
     );
-  });
+
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+
+    return { success: false, error };
+  }
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   // Require that this request is authenticated!
 
   const { headers } = req; // https://stackoverflow.com/a/63529345/470749
-  if (headers?.[apiKeyHeaderName] === apiKey) {
-    const body = req?.body;
-    console.log({ headers, body });
-    const { accountId } = body; // Eventually we will want to add error-handling / validation.
-    try {
-      const result = await invalidateAllCertsForAccount(accountId);
-      res.status(HTTP_SUCCESS).json({ result });
-    } catch (err) {
-      console.error(err);
-      res.status(HTTP_ERROR).json({ status: 'error', message: 'invalidateAllCertsForAccount failed.' });
-    }
-  } else {
+  if (headers?.[apiKeyHeaderName] !== apiKey) {
     rejectAsUnauthorized(res, headers);
+  }
+
+  const body = req?.body;
+  console.log({ headers, body });
+  const { accountId } = body; // Eventually we will want to add error-handling / validation.
+
+  try {
+    const result = await invalidateAllCertsForAccount(accountId);
+    res.status(HTTP_SUCCESS).json({ result });
+  } catch (err) {
+    console.error(err);
+    res.status(HTTP_ERROR).json({ status: 'error', message: 'invalidateAllCertsForAccount failed.' });
   }
 }
