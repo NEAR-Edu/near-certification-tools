e38c0c52a0a27eb7fc49adb23d04709b663692db
Esin Adsoy <esin.adsoy@gmail.com>
2022-04-20 11:07:15 +0300

    refactor generateActivityData to be able to generate data for given account in different intervals and time units such as day, hour, etc.

--- web-app/test/test-helpers/generate-account-activities.ts
+++ web-app/test/test-helpers/generate-account-activities.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto'; // https://stackoverflow.com/a/27747377
 import utc from 'dayjs/plugin/utc'; // https://day.js.org/docs/en/plugin/utc
 import { convertStringDateToNanoseconds } from '../../helpers/time';
 
-dayjs.extend(utc); // use dayjs utc plugin to avoid parsing different dates depending on local timezone.
+dayjs.extend(utc); // Use dayjs utc plugin to avoid parsing different dates depending on local timezone.
 
 type ActivityData = {
   signer_account_id: string;
@@ -13,34 +13,18 @@ type ActivityData = {
   }[];
 };
 
-export async function generateActivityData(data: ActivityData, startDate: string, endDate: string) {
+export default async function generateActivityData(data: ActivityData, startDate: string, endDate: string, timeUnit: any, interval: number) {
   const startDateDayJs = dayjs.utc(startDate);
   const endDateDayJs = dayjs.utc(endDate);
-  const duration = endDateDayJs.diff(startDateDayJs, 'days'); // amount of days between startDate and endDate
+  const duration = endDateDayJs.diff(startDateDayJs, timeUnit); // Duration between startDate and endDate in timeUnit (i.e. days, hours,...)
 
-  // add activity every 5 days
-  for (let i = 5; i < duration; i += 5) {
-    const date = startDateDayJs.add(i, 'day').format('YYYY-MM-DDTHH:mm:ss+00:00');
-
-    data.account_activities.push({
-      included_in_block_timestamp: convertStringDateToNanoseconds(date),
-      receipt_id: crypto.randomBytes(22.5).toString('hex'), // match receipt_id length convention of 45 chars. Using randomBytes, resulting string is double the size of given bytes in length.
-    });
-  }
-}
-
-// TODO: refactor function for other cases
-// Creates activity data so that the expiration date calculation returns today's date one hour from now
-export async function generateDynamicActivityData(data: ActivityData) {
-  const startDateDayJs = dayjs.utc().subtract(180, 'day').subtract(6, 'hour');
-
-  // add activity every 1 hour
-  for (let i = 0; i <= 7; i += 1) {
-    const date = startDateDayJs.add(i, 'hour').format('YYYY-MM-DDTHH:mm:ss+00:00');
+  // add activity every value of interval in given timeUnit
+  for (let i = interval; i < duration; i += interval) {
+    const date = startDateDayJs.add(i, timeUnit).format('YYYY-MM-DDTHH:mm:ss+00:00');
 
     data.account_activities.push({
       included_in_block_timestamp: convertStringDateToNanoseconds(date),
-      receipt_id: crypto.randomBytes(22.5).toString('hex'), // match receipt_id length convention of 45 chars. Using randomBytes, resulting string is double the size of given bytes in length.
+      receipt_id: crypto.randomBytes(22.5).toString('hex'), // Match receipt_id length convention of 45 chars. Using randomBytes, resulting string is double the size of given bytes in length.
     });
   }
 }
