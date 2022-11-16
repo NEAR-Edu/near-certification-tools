cc830d97603f2bedfba49e6bf5b01369b44f0f31
Petar Vujović <petarvujovic98@gmail.com>
2022-11-16 12:35:55 +0100

    chore(web-app): Refactor certification invalidation

--- web-app/pages/api/invalidate-all-certs-for-account.ts
+++ web-app/pages/api/invalidate-all-certs-for-account.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { type NextApiRequest, type NextApiResponse } from 'next';
+import { z, ZodError } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+import { checkAccountId } from '../../helpers/certificate';
 import { type AccountId, apiKey, gas, getNftContract, HTTP_ERROR, HTTP_SUCCESS, rejectAsUnauthorized } from '../../helpers/near';
 import { type JsonResponse } from '../../helpers/types';
 
@@ -11,10 +14,10 @@ async function invalidateAllCertsForAccount(accountId: AccountId) {
 
   try {
     await Promise.all(
-      tokens.map((certificate) =>
+      tokens.map(({ token_id }) =>
         contract.cert_invalidate(
           // https://github.com/near/near-api-js/issues/719
-          { token_id: certificate.token_id }, // `memo` here?
+          { token_id }, // `memo` here?
           gas,
           '1', // deposit exactly 1 yoctoNEAR
         ),
@@ -29,23 +32,47 @@ async function invalidateAllCertsForAccount(accountId: AccountId) {
   }
 }
 
-export default async function handler(request: NextApiRequest, response: NextApiResponse<JsonResponse>) {
-  // Require that this request is authenticated!
+function validateInput(input: unknown): string {
+  const validationSchema = z.object({
+    accountId: z.string().min(1).refine(checkAccountId, { message: 'Not a valid account ID.' }),
+  });
 
-  const { headers } = request; // https://stackoverflow.com/a/63529345/470749
+  const result = validationSchema.parse(input);
+
+  return result.accountId;
+}
+
+export default async function handler({ headers, body }: NextApiRequest, response: NextApiResponse<JsonResponse>) {
+  // Require that this request is authenticated!
   if (headers[apiKeyHeaderName] !== apiKey) {
     rejectAsUnauthorized(response, headers);
+    return;
   }
 
-  const { body } = request;
   console.log({ body, headers });
-  const { accountId } = body; // Eventually we will want to add error-handling / validation.
+  let accountId: string;
+
+  try {
+    accountId = validateInput(body);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const formatted = fromZodError(error);
+      console.error(formatted);
+      response.status(400).json({ message: JSON.stringify(formatted), status: 'error' });
+    }
+
+    response.status(400).json({ message: `Invalid data. ${JSON.stringify(error)}`, status: 'error' });
+
+    return;
+  }
 
   try {
     const result = await invalidateAllCertsForAccount(accountId);
+
     response.status(HTTP_SUCCESS).json(result);
   } catch (error) {
     console.error(error);
+
     response.status(HTTP_ERROR).json({ message: 'invalidateAllCertsForAccount failed.', status: 'error' });
   }
 }
